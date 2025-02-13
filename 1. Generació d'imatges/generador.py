beb3aa99d5102317bd446302981b0c6e4ad9503c
dsanchezf02 <154837658+dsanchezf02@users.noreply.github.com>
2025-02-13 11:02:59 +0100

    Update generador.py

--- 1. Generació d'imatges/generador.py
+++ 1. Generació d'imatges/generador.py
@@ -4,7 +4,7 @@ import datetime
 device = "cuda"
 
 # Introduir ruta del model de generació
-pipeline = DDIMPipeline.from_pretrained(f"/Users/diegosanchezfreire/Desktop/DISCRECIÓN MÁXIMA/ENTREGA TFG/Prototip/1. Generació d'imatges/MODEL", local_files_only=True)
+pipeline = DDIMPipeline.from_pretrained(f"CARPETA MODEL", local_files_only=True)
 pipeline.scheduler = DDIMScheduler.from_config(pipeline.scheduler.config)
 pipeline.to(device)
 
@@ -14,5 +14,5 @@ image = pipeline(num_inference_steps=100).images[0]
 current_datetime = datetime.datetime.now().strftime("%Y%m%d%H%M")
 
 # Directori on es guardaran les imatges
-filename = f"/Users/diegosanchezfreire/Desktop/DISCRECIÓN MÁXIMA/ENTREGA TFG/Prototip/1. Generació d'imatges/RESULTATS"
+filename = f"CARPETA RESULTATS"
 image.save(filename)
